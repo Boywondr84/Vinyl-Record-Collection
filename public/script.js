5597cc9b8252d01e2b4e7a0477710c097459707a
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-11-30 01:12:14 -0600

    added form for artist search

--- public/script.js
+++ public/script.js
@@ -1,17 +1,27 @@
-var fetchButton = document.getElementById('fetch-button');
+var artistFormEl = document.querySelector('#artist-form')
+var findArtistEl = document.querySelector('#find-artist');
+var fetchButton = document.querySelector('#submit-button');
 var groupNameEl = document.getElementById('group-name');
+
 
 function getApi() {
 
-    var requestUrl = 'https://musicbrainz.org/ws/2/artist?fmt=json&query=pink+floyd&limit=2';
+    var requestUrl = 'https://musicbrainz.org/ws/2/artist?fmt=json&query=rush&limit=2';
     fetch(requestUrl)
         .then((response) => response.json())
-        .then(function(data) {
-                console.log(data.artists[0].name, data.artists[0].id)
-                var listItem = document.createElement('li');
-                listItem.textContent = data.artists[0].name;
-                groupNameEl.appendChild(listItem);
-            })
-        }
+        .then(function (data) {
+            console.log(data.artists[0].name, data.artists[0].id)
+            var listItem = document.createElement('li');
+            listItem.textContent = data.artists[0].name;
+            groupNameEl.appendChild(listItem);
+        })
+}
 
-fetchButton.addEventListener('click', getApi);
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var artistName = findArtistEl.value;
+    console.log(artistName);
+    findArtistEl.value = "";
+}
+
+artistFormEl.addEventListener('submit', formSubmitHandler);
