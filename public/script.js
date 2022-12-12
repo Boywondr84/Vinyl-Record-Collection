7ba12fbce65b664f578ffab7424e6d5fa7b93210
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-12-11 23:46:11 -0600

    fetching albums using artist ID retrieved from artist name search

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ var artistFormEl = document.querySelector('#artist-form')
 var findArtistEl = document.querySelector('#find-artist');
 var fetchButton = document.querySelector('#submit-button');
 var groupNameEl = document.getElementById('group-name');
-var albumImageEl = document.querySelector('#album-image');
+var albumListEl = document.querySelector('#album-list');
 var artistIdNumber = '';
 
 // var res = fetch('https://itunes.apple.com/search?term=166529&entity=album')
@@ -14,58 +14,39 @@ var artistIdNumber = '';
 
 function getApi(artistName) {
 
-    var requestUrl = 'https://itunes.apple.com/search?term=' + artistName + '&entity=album';
+    var requestUrl = 'https://itunes.apple.com/search?term=' + artistName;
+    // + '&entity=album&limit=1';
     // 'https://musicbrainz.org/ws/2/artist?fmt=json&query=' '&limit=2';
     fetch(requestUrl)
         .then((response) => response.json())
         .then(function (data) {
             // console.log(data)
-            
+
             for (var i = 0; i < data.results.length; i++) {
                 var filteredArtistName = data.results.filter(function (element) {
-                    if (element.artistName != artistName) {
-                        var errorArtistName = document.createElement('p');
-                        errorArtistName.textContent = "Check spelling and make sure the first letter(s) are capitalized"
-                        groupNameEl.appendChild(errorArtistName);
-                    }
                     return (element.artistName === artistName);
-                    
                 });
-                
-                console.log(filteredArtistName);
-                console.log(filteredArtistName[0].artistName + ' has an ID number of ' + filteredArtistName[0].artistId);
-
-
-                // after finding artistId, use this id to filter out albums.
-                // var artistIdNumbers = [data.results[i].artistId];
-                // console.log(artistIdNumbers);
-                // const filteredArtistIdNumbers = artistIdNumbers.filter(function (data) {
-                    // return data === 166529;
-                // });
-                // console.log(filteredArtistIdNumbers);
+                // find albums using artist ID
             }
-
-            // console.log(data.results[0].artistName + ' has an ID of ')
-            // console.log('Album: ' + data.results[2].collectionName + ' has an ID of ' + data.results[2].collectionId)
-            
-            // var artistIdNum = data.results[0].artistId;
-            // getAlbums();
+            var artistIdNumber = filteredArtistName[0].artistId;
             var listItem = document.createElement('li');
-            listItem.textContent = artistName + ' has an ID number of ' + filteredArtistName[0].artistId;
+            listItem.textContent = artistName + ' has an ID number of ' + artistIdNumber;
             groupNameEl.appendChild(listItem);
-            // var albumCover = document.createElement('div');
-            // albumCover.textContent = filteredArtistName[0].artworkUrl100;
-            // albumImageEl.appendChild(albumCover);
-        })
+            getAlbums();
+            // var albumListItem = document.createElement('li');
+            // albumListItem.textContent = 
+
+            function getAlbums() {
+                var albumRequestUrl = 'https://itunes.apple.com/lookup?id=' + artistIdNumber + '&entity=album&limit=10';
+
+                fetch(albumRequestUrl)
+                    .then((response) => response.json())
+                    .then(function (data) {
+                        console.log(data);
+                    })
+            }
+        });
 }
-// function getAlbums(artistIdNum) {
-//     var albumRequestUrl = 'https://itunes.apple.com/us/artist' + artistName + artistIdNum + '/see-all?section=full-albums';
-//         fetch(albumRequestUrl)
-//         .then((response) => response.json())
-//         .then(function (data) {
-//             console.log(data);
-//         })
-// }
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
