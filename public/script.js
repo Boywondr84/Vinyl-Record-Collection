f068b03fcde6923fb385b26270f1066b82e7febf
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-12-24 02:00:57 -0600

    trying to use music brainz exclusively. Think I can fetch all the info I want. Need to still figure out filtering it and then displaying.

--- public/script.js
+++ public/script.js
@@ -5,54 +5,67 @@ var groupNameEl = document.getElementById('group-name');
 var albumListEl = document.querySelector('#album-list');
 var artistIdNumber = '';
 
-// var res = fetch('https://itunes.apple.com/search?term=166529&entity=album')
-//     // 'https://musicbrainz.org/ws/2/release/42e47930-3707-347c-9f1a-0e04e0bc5355')
-// .then((response) => response.json())
-// .then((data) => console.log(data));
 
+// USING MusicBrainz
+function getArtist(artist) {
 
-
-function getApi(artistName) {
-
-    var requestUrl = 'https://itunes.apple.com/search?term=' + artistName;
-    // + '&entity=album&limit=1';
-    // 'https://musicbrainz.org/ws/2/artist?fmt=json&query=' '&limit=2';
-    fetch(requestUrl)
+    var res =
+        // 'https://itunes.apple.com/search?term=166529&entity=album'
+        'https://musicbrainz.org/ws/2/artist?fmt=json&query=' + artist + '&limit=1';
+    fetch(res)
         .then((response) => response.json())
         .then(function (data) {
-            // console.log(data)
+            for (var i = 0; i < data.artists.length; i++) {
+                data.artists.filter(function (element) {
+                    if (element.name === artist && element.score === 100);
+                    console.log(element.name + ' has an ID of ' + element.id);
 
-            for (var i = 0; i < data.results.length; i++) {
-                var filteredArtistName = data.results.filter(function (element) {
-                    return (element.artistName === artistName);
+                    // var artistIdNumber = filteredArtistName[0].id;
+                    // var listItem = document.createElement('li');
+                    // listItem.textContent = artist + ' has an ID number of ' + artistIdNumber;
+                    // groupNameEl.appendChild(listItem);
+                    getAlbums();
+
+                    function getAlbums() {
+                        var albumRequestUrl =
+                            // 'https://itunes.apple.com/lookup?id=' + artistIdNumber + '&entity=album&limit=15';
+                            'https://musicbrainz.org/ws/2/release?fmt=json&query=' + artist + '&limit=15';
+
+                        fetch(albumRequestUrl)
+                            .then((response) => response.json())
+                            .then(function (data) {
+                                for (var i = 0; i < data.releases.length; i <= 25) {
+                                    data.releases.filter(function (element) {
+                                        if (element.status === 'Official' && element.country === 'US')
+                                            console.log(element.id, element.media[0].format, element.date);
+                                    })
+                                }
+
+
+                                // for (var i = 1; i < data.results.length; i++) {
+                                //     var artistAlbums = data.results[i].collectionName;
+                                //     // var artistAlbumsArtwork = data.results[i].artworkUrl100;
+                                //     console.log(artistAlbums);
+                                //     // console.log(artistAlbumsArtwork);
+
+                                //     var artistAlbumList = document.createElement('ul');
+                                //     artistAlbumList.textContent = artistAlbums;
+                                //     albumListEl.appendChild(artistAlbumList);
+
+                            })
+                    }
                 });
-                // find albums using artist ID
             }
-            var artistIdNumber = filteredArtistName[0].artistId;
-            var listItem = document.createElement('li');
-            listItem.textContent = artistName + ' has an ID number of ' + artistIdNumber;
-            groupNameEl.appendChild(listItem);
-            getAlbums();
-            // var albumListItem = document.createElement('li');
-            // albumListItem.textContent = 
-
-            function getAlbums() {
-                var albumRequestUrl = 'https://itunes.apple.com/lookup?id=' + artistIdNumber + '&entity=album&limit=10';
-
-                fetch(albumRequestUrl)
-                    .then((response) => response.json())
-                    .then(function (data) {
-                        console.log(data);
-                    })
-            }
-        });
+        })
 }
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
-    var artistName = findArtistEl.value;
+    // var artistName = findArtistEl.value;
+    var artist = findArtistEl.value;
     // console.log(artistName);
-    getApi(artistName);
+    // getApi(artistName);
+    getArtist(artist);
     findArtistEl.value = "";
 }
 
