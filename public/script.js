55a2866513640e5e8674d8657ddd4a7bfd7f9f37
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-12-28 15:55:27 -0600

    artist name dynamically added to page and Albums with year also dynamically added.

--- public/script.js
+++ public/script.js
@@ -16,63 +16,58 @@ function getArtist(artist) {
         .then((response) => response.json())
         .then(function (data) {
             for (var i = 0; i < data.artists.length; i++) {
-                data.artists.filter(function (element) {
-                    if (element.name === artist && element.score === 100);
-                    console.log(element.name + ' has an ID of ' + element.id);
-                    var artistIdNumber = element.id;
-                    // console.log(data);
+                if (data.artists.name === artist && data.score === 100);
+                console.log(artist + ' has a MusicBrainz ID of ' + data.artists[0].id);
+                var artistIdNumber = data.artists[0].id;
+                var listItem = document.createElement('h2');
+                listItem.textContent = 'Albums recorded by ' + artist + ' are:';
+                groupNameEl.appendChild(listItem);
+                // console.log(data);
+
+                getAlbums();
+
+                function getAlbums() {
+                    var albumRequestUrl =
+                        // 'https://itunes.apple.com/lookup?id=' + artistIdNumber + '&entity=album&limit=15';
+                        'https://musicbrainz.org/ws/2/artist/' + artistIdNumber + '?fmt=json&inc=release-groups';
+
+                    fetch(albumRequestUrl)
+                        .then((response) => response.json())
+                        .then(function (data) {
+                            // console.log(data['release-groups'][0].title);
+
+                            // HOW CAN I GRAB TITLE DATA FROM RELEASE-GROUPS???
+                            // ANSWER -- use ['-']
+                            for (var i = 0; i < data['release-groups'].length; i++) {
+                                if (data['release-groups'][i]['primary-type'] === 'Album');
+                                var artistAlbumsTitle = data['release-groups'][i].title;
+                                var artistAlbumsId = data['release-groups'][i].id;
+                                var artistAlbumsYear = data['release-groups'][i]['first-release-date'].slice(0, 4);
+
+                                var artistAlbumList = document.createElement('ul');
+                                artistAlbumList.textContent = artistAlbumsTitle + ' released in  ' + artistAlbumsYear;
+                                albumListEl.appendChild(artistAlbumList);
+                                // console.log(artistAlbumsTitle, artistAlbumsYear, artistAlbumsId)
+                            }
+
+                            //     data.release-groups.forEach(function (element) {
+                            //         if (element.status === 'Official' && element.packaging === 'Cardboard/Paper Sleeve' && element.primary-type === 'Album')
+                            //         console.log(element);
+                            //         console.log(element.id, element.first-release-date);
+
+                            //         if (element.country == "US" && element.media[0].format === '12\" Vinyl')
+                            //             console.log(element.id, element.media, element.date);
+                            // })
+                            // }
 
 
-                    // var artistIdNumber = filteredArtistName[0].id;
-                    // var listItem = document.createElement('li');
-                    // listItem.textContent = artist + ' has an ID number of ' + artistIdNumber;
-                    // groupNameEl.appendChild(listItem);
-                    getAlbums();
-
-                    function getAlbums() {
-                        var albumRequestUrl =
-                            // 'https://itunes.apple.com/lookup?id=' + artistIdNumber + '&entity=album&limit=15';
-                            'https://musicbrainz.org/ws/2/artist/' + artistIdNumber + '?fmt=json&inc=release-groups';
-
-                        fetch(albumRequestUrl)
-                            .then((response) => response.json())
-                            .then(function (data) {
-                                // console.log(data['release-groups'][0].title);
-
-                                // HOW CAN I GRAB TITLE DATA FROM RELEASE-GROUPS???
-                                // ANSWER -- use ['-']
-                                for (var i = 0; i < data['release-groups'].length; i++) {
-                                    if (data['release-groups'][i]['primary-type'] === 'Album')
-                                    console.log(data['release-groups'][i].title, data['release-groups'][i].id);
-                                }
-                                
-                                // console.log(data.releasegroups[0].title)
-                                // albums = data.release-groups[i].title;
-                                
-                                //     data.release-groups.forEach(function (element) {
-                                //         if (element.status === 'Official' && element.packaging === 'Cardboard/Paper Sleeve' && element.primary-type === 'Album')
-                                //         console.log(element);
-                                //         console.log(element.id, element.first-release-date);
-                                
-                                //         if (element.country == "US" && element.media[0].format === '12\" Vinyl')
-                                //             console.log(element.id, element.media, element.date);
-                                    // })
-                                // }
-
-
-                                // for (var i = 1; i < data.results.length; i++) {
-                                //     var artistAlbums = data.results[i].collectionName;
-                                //     // var artistAlbumsArtwork = data.results[i].artworkUrl100;
-                                //     console.log(artistAlbums);
-                                //     // console.log(artistAlbumsArtwork);
-
-                                //     var artistAlbumList = document.createElement('ul');
-                                //     artistAlbumList.textContent = artistAlbums;
-                                //     albumListEl.appendChild(artistAlbumList);
-
-                            })
-                    }
-                });
+                            // for (var i = 1; i < data.results.length; i++) {
+                            //     var artistAlbums = data.results[i].collectionName;
+                            //     // var artistAlbumsArtwork = data.results[i].artworkUrl100;
+                            //     console.log(artistAlbums);
+                            //     // console.log(artistAlbumsArtwork);
+                        })
+                }
             }
         })
 }
